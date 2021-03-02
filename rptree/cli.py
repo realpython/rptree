697e3d83dbed72b16f5de5017d74b176baaf4a7d
Leodanis Pozo Ramos <lpozor78@gmail.com>
2021-03-01 20:41:12 -0500

    Remove main() from cli.py

--- rptree/cli.py
+++ rptree/cli.py
@@ -1,23 +1,9 @@
 """This module provides the RP Tree CLI."""
 
 import argparse
-import pathlib
 import sys
 
 from . import __version__
-from .rptree import DirectoryTree
-
-
-def main():
-    args = parse_cmd_line_arguments()
-    root_dir = pathlib.Path(args.root_dir)
-    if not root_dir.is_dir():
-        print("The specified root directory doesn't exist")
-        sys.exit()
-    tree = DirectoryTree(
-        root_dir, dir_only=args.dir_only, output_file=args.output_file
-    )
-    tree.generate()
 
 
 def parse_cmd_line_arguments():
